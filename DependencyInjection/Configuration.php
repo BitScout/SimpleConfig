5e3ece78008645ed929eca2422a020f787eb2eaa
Christian Kollross <christian@kollross.name>
2020-11-12 22:00:15 +0100

    Fixed spelling mistake in Configuration.php

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Bitscout\SimpleConfig\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder('bitscout_simple_config');
+        $rootNode    = $treeBuilder->getRootNode();
+
+        $rootNode
+            ->children()
+                ->arrayNode('fields')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->scalarNode('label')->isRequired()->cannotBeEmpty()->end()
+                        ->scalarNode('type')->isRequired()->cannotBeEmpty()->end()
+                        ->booleanNode('show')->defaultTrue()->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
